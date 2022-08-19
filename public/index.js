81d9981840ff4852b66704a89590f18b8697470f
Manuel Estrada <manuel.estrada16@gmail.com>
2022-08-19 10:22:46 -0400

    initial commit

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
-    let countValue = 0;
+    var response = await fetch('http://localhost:9001/counter');
+    var jsonResponse = await response.json();
+    let countValue = jsonResponse.value;
 
     function increment(){
         countValue++;
@@ -15,8 +16,14 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    /*function refresh() {
+        let refreshValue = countValue;
+        countContainer.textContent = refreshValue;
+    }*/
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    //refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
